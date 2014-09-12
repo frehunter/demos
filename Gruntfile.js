746e228c72ee023559f668f85f5e53a715e7d139
Bin Du <bindu@ksimple.org>
2014-09-12 09:08:53 +0800

    .

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,15 +5,21 @@ module.exports = function(grunt) {
     var projects = grunt.file.expand('src/*/Gruntfile.js');
     var prepareTasks = [];
     var defaultTasks = [];
+    var installTasks = [];
 
     grunt.task.registerMultiTask('setBase', function () {
         grunt.file.setBase(this.data.base);
     });
 
+    var buildDir = path.join(process.cwd(), grunt.option('builddir') || 'build');
+    var installDir = path.join(process.cwd(), grunt.option('installdir') || 'install');
+
     for (var index in projects) {
         var name = path.basename(path.dirname(projects[index]));
+
         grunt.config.set(['shell', name + '.prepare', 'command'], ['npm install', 'grunt prepare'].join('&'));
-        grunt.config.set(['shell', name + '.default', 'command'], 'grunt');
+        grunt.config.set(['shell', name + '.default', 'command'], 'grunt --builddir=' + path.join(buildDir, name) + ' --installdir=' + path.join(installDir, name));
+        grunt.config.set(['shell', name + '.install', 'command'], 'grunt install --builddir=' + path.join(buildDir, name) + ' --installdir=' + path.join(installDir, name));
         grunt.config.set(['npm-install', name], {});
         grunt.config.set(['setBase', name, 'base'], 'src/' + name);
 
@@ -22,8 +28,12 @@ module.exports = function(grunt) {
 
         defaultTasks.push('setBase:' + name);
         defaultTasks.push('shell:' + name + '.default');
+
+        installTasks.push('setBase:' + name);
+        installTasks.push('shell:' + name + '.install');
     }
 
+    grunt.registerTask('install', installTasks);
     grunt.registerTask('default', defaultTasks);
     grunt.registerTask('prepare', prepareTasks);
 };
--- src/listview/Gruntfile.js
+++ src/listview/Gruntfile.js
@@ -1,15 +1,28 @@
 module.exports = function(grunt) {
     require('load-grunt-tasks')(grunt);
+    var path = require('path');
+
+    var buildDir = grunt.option('builddir') || 'build';
+    var installDir = grunt.option('installdir') || 'install';
 
     grunt.initConfig({
         bower: {
             install: {
             }
-        }
+        },
+        copy: {
+            install: {
+                files: [
+                    {expand: true, src: ['*.html', 'scripts/*.js'], dest: installDir, filter: 'isFile'},
+                    {expand: true, src: ['lib/**/*'], dest: path.join(installDir, 'scripts'), filter: 'isFile'},
+                ],
+            },
+        },
     });
 
     // A very basic default task.
     grunt.registerTask('prepare', ['bower:install']);
     grunt.registerTask('default', []);
+    grunt.registerTask('install', ['copy:install']);
 };
 
